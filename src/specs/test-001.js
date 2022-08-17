bbdba77d49b9cdd3530dd2356a6510a9aafaf98e
Nguyen Nhu <>
2022-08-17 11:46:27 +0700

    initial commit comment

---
+++ src/specs/test-001.js
@@ -0,0 +1,7 @@
+describe('Metamask', () => {
+      it('test metamask', () => {
+        cy.visit('https://www.google.com')
+        cy.log("nhutest")
+        //cy.get('#connectButton').click();
+      })
+    })
